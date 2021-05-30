8f11c38df190481bec354f06e5132f701332db64
douglas <douglascardoso@usp.br>
2021-05-30 09:34:32 -0300

    using R to extract data stocks

---
+++ finance/case-dev/get_data_stocks.R
@@ -0,0 +1,81 @@
+# library
+# https://www.codingfinance.com/post/2018-03-27-download-price/
+library(tidyquant)
+library(tidyverse)
+library(glue)
+
+# read file
+composition <- read_csv('data/composicao_ibx_mensal.csv')
+
+# get unique ticks
+unq_ticks <- unique(unlist(composition[-1]))
+
+# remove na
+unq_ticks <- unq_ticks[!unq_ticks %in% NA]
+
+# put '.SA' in ticks 
+ticks <- map_chr(unq_ticks, ~glue::glue('{.x}.SA'))
+
+
+# get data
+extern_api <- function(x){
+  
+  getSymbols(x, src = 'yahoo',
+             
+             from='2010-12-01', to='2020-02-01', 
+             
+             periodicity='monthly')
+  
+}
+
+safety_api <- possibly(extern_api, otherwise = NA_real_)
+
+# apenas ticks reconhecidos  
+accepted_ticks <- map_chr(ticks, safety_api)
+
+# remove NA
+accepted_ticks <- accepted_ticks[!accepted_ticks %in% NA]
+
+
+# aplicando 
+getSymbols(accepted_ticks, src = 'yahoo',
+           
+           from='2010-12-01', to='2020-02-01', 
+           
+           periodicity='monthly')
+
+# extract prices
+prices <- map(accepted_ticks, ~Ad(get(.x)))
+
+prices <- reduce(prices, merge)  
+
+colnames(prices) <- accepted_ticks
+  
+# extract volume
+
+volume <- map(accepted_ticks, ~Vo(get(.x)))
+
+volume <- reduce(volume, merge)
+
+colnames(volume) <- accepted_ticks 
+
+# export results
+"""
+write.zoo(prices, file = 'data/prices.csv', sep = ',')
+write.zoo(volume, file = 'data/volume.csv', sep = ',')
+"""
+get_returns <- function(x, type = 'arithmetic',  period = 'monthly'){
+  
+  periodReturn(get(x)[, glue('{x}.Adjusted')], period = period, subset=NULL, type=type, leading = TRUE)
+  
+}
+
+
+# percent returns 
+pct_returns <- map(accepted_ticks, ~get_returns(.x))
+
+# log returns
+log_returns <- map(accepted_ticks, ~get_returns(.x, 'log'))
+
+
+map(accepted_ticks, ~get_returns(.x, period = 'yearly'))
